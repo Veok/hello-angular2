02292e304705127766eaf22f3928777a2ff3cf0b
Lelental <p.lelental@gmail.com>
2017-03-07 20:26:37 +0100

    removing node_modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+
+  public myTitles: Array < string > = [];
+
+  private _title: string;
+  public get title(): string {
+    return this._title;
+  }
+  public set title(v: string) {
+    this._title = v;
+  }
+
+  public addTitle(): void {
+    this.myTitles.push(this._title);
+    this._title = '';
+  }
+  public constructor() {
+    this._title = "Gitara Siema!";
+  }
+
+  public ngOnInit(): void {}
+
+
+
 }
---
+++ src/app/title/title.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+	selector: 'title-children',
+	templateUrl: 'title.component.html',
+	styleUrls:['title.component.css']
+})
+
+export class TitleComponent implements OnInit {
+
+	@Input() myTitle:string;
+
+	public isVisible:
