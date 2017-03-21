1baf8671c1490e546855192d4e657963b275c027
Lelental <p.lelental@gmail.com>
2017-03-21 18:30:37 +0100

    added edit-title component

--- src/app/title-module/edit-title/edit-title.component.ts
+++ src/app/title-module/edit-title/edit-title.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import 'rxjs/add/operator/switchMap';
 
 @Component({
 	selector: 'edit-title',
@@ -7,5 +9,19 @@ import { Component, OnInit } from '@angular/core';
 
 export class EditTitleComponent implements OnInit {
 
-	ngOnInit() { }
+	public title:string = "";
+
+	constructor(private router:Router, private route:ActivatedRoute){
+		router.config
+	}
+
+	ngOnInit() { 
+
+	this.route.params
+	.switchMap((params:Params)=>params['title'])
+	.subscribe(
+		(t:string)=>
+			this.title+=t
+		);
+	}
 }
--- src/app/title-module/title.module.ts
+++ src/app/title-module/title.module.ts
@@ -6,14 +6,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TitleComponent } from "./components/title/title.component";
 import { TitleListComponent } from "./components/title-list/title-list.component";
 import { AddTitleComponent } from "./components/add-title/add-title.component";
+import { Routes, RouterModule } from "@angular/router";
+import { EditTitleComponent } from "./edit-title/edit-title.component";
 
+const routes:Routes = [
+    {path:'titles/title/:title', component:EditTitleComponent}
+];
 @NgModule({
     imports: [
         FormsModule,
         BrowserModule,
-        CommonModule
+        CommonModule,
+        RouterModule.forChild(routes)
         ],
-    exports: [TitleComponent, TitleListComponent, AddTitleComponent],
+    exports: [TitleComponent, TitleListComponent, AddTitleComponent, EditTitleComponent],
     declarations: [TitleComponent, TitleListComponent, AddTitleComponent],
     providers: [],
     schemas: []
